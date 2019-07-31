ac0261b1b53fd0261318758aa93b836ddf399aa0
ashwingajera <gajeraashwin2@gmail.com>
2019-08-01 00:05:02 +0100

    first commit

---
+++ src/PersonalDetail.java
@@ -0,0 +1,38 @@
+public class PersonalDetail
+
+{ public static void main (String []args) {
+
+
+    String name = "ashwingajera";
+    int i = 10;
+    String address = "53 featherdell hatfield al10 8ue";
+    String mobilenumber = "07854102135";
+    long l = 2800;
+
+
+
+
+
+    System.out.println(name);
+    System.out.println(i);
+    System.out.println(address);
+    System.out.println(mobilenumber);
+    System.out.println(l);
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    String name = "ashwingajera" ;
+        int i = 10 ;
+
+    public static void main(String[] args) {
+
+        String name = "ashwingajera" ;
+        int i = 33 ;
+
+
+        System.out.println("ashwingajera");
+	// write your code here
+        System.out.println("33");
+        System.out.println("57 featherdell al7 3eu");
+        System.out.println("07854102135");
+        System.out.println("£28000 per year");
+        System.out.println("name");
+        System.out.println();
+
+        System.out.println("name");
+        System.out.println("i");
+
+    }
+}
